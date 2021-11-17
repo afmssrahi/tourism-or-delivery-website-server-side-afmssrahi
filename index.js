7f2c315746e0a0cd7f96c755554058b98947a02b
afmssrahi <afmssrahi@gmail.com>
2021-11-17 21:56:49 +0600

    All API Working

--- index.js
+++ index.js
@@ -18,13 +18,89 @@ const client = new MongoClient(uri, {
 	useUnifiedTopology: true,
 });
 
-// async function run() {
-// 	try {
-// 	} finally {
+async function run() {
+	try {
+		await client.connect();
+		const database = client.db('travelBari');
+		const serviceCollection = database.collection('services');
+		const bookingCollection = database.collection('bookings');
 
-// 	}
-// }
-// run().catch(console.dir);
+		// GET all services API
+		app.get('/services', async (req, res) => {
+			const cursor = serviceCollection.find({});
+			const services = await cursor.toArray();
+			res.json(services);
+		});
+
+		// GET single service API
+		app.get('/services/:id', async (req, res) => {
+			const id = req.params.id;
+			const query = { _id: ObjectId(id) };
+			const service = await serviceCollection.findOne(query);
+			res.json(service);
+		});
+
+		// POST service API
+		app.post('/services', async (req, res) => {
+			const service = req.body;
+			const result = await serviceCollection.insertOne(service);
+			console.log(result);
+			res.send(result);
+		});
+
+		// GET all booked tour API
+		app.get('/bookings', async (req, res) => {
+			const cursor = bookingCollection.find({});
+			const bookedTours = await cursor.toArray();
+			res.json(bookedTours);
+		});
+
+		// GET Logged in user's booked tours
+		app.get('/bookings/:email', async (req, res) => {
+			const email = req.params.email;
+			const query = { email: email };
+			const cursor = bookingCollection.find(query);
+			const bookings = await cursor.toArray();
+			res.json(bookings);
+		});
+
+		// POST booking API
+		app.post('/bookings', async (req, res) => {
+			const booking = req.body;
+			const result = await bookingCollection.insertOne(booking);
+			res.send(result);
+		});
+
+		// DELETE API
+		app.delete('/bookings/:id', async (req, res) => {
+			const id = req.params.id;
+			const query = { _id: ObjectId(id) };
+			const result = await bookingCollection.deleteOne(query);
+			res.send(result);
+		});
+
+		// Update API
+		app.put('/bookings/:id', async (req, res) => {
+			const id = req.params.id;
+			const filter = { _id: ObjectId(id) };
+			const options = { upsert: true };
+			const updateDoc = {
+				$set: {
+					status: 'approved',
+				},
+			};
+			const result = await bookingCollection.updateOne(
+				filter,
+				updateDoc,
+				options
+			);
+			res.send(result);
+		});
+	} finally {
+		//   await client.close();
+	}
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
 	res.send('Running my Travel Bari Server');
